20d082873e629b2db76fc3ad72e3bbc15f0ae92e
missefa <erfab@hotmail.com>
2023-03-29 01:40:39 -0300

    commit2

---
+++ Ej3_guiaTeorica2/src/ej3_guiateorica2/Ej3_guiaTeorica2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej3_guiateorica2;
+
+/**
+ *
+ * @author Erica
+ */
+public class Ej3_guiaTeorica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int num1 = 101;
+       double num2 = 20.2;
+       double division = num1 / num2;
+       int mod = (int) (num1 % num2);
+       boolean logico1 = num1 >= num2;
+       num1++;
+        double num3 = -num2;
+       
+        System.out.println(division + " " + mod + " " + logico1 + " " + num1++ + " " + num3);
+    }
+    
+}
