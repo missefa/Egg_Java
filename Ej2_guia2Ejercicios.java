98815e1df701ec6179b6e1ecdd0aca839ec56038
EF EE <ericafabbro@yahoo.com.ar>
2023-03-30 18:43:48 -0300

    commit 4

---
+++ Ej2_guia2Ejercicios.java
@@ -0,0 +1,24 @@
+/*
+Escribir un programa que pida tu nombre, lo guarde en una variable y lo
+muestre por pantalla.
+ */
+package ej2_guia2ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Ej2_guia2Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner ingresar = new Scanner(System.in);
+    String nombre = ingresar.nextstring();
+    
+    }
+    
+}
