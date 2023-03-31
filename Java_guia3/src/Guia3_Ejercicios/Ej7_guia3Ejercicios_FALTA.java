41395e7b9ee259e581cd7fce941756029e6e5eaa
missefa <erfab@hotmail.com>
2023-03-31 01:39:07 -0300

    commit 6

---
+++ Java_guia3/src/Guia3_Ejercicios/Ej7_guia3Ejercicios_FALTA.java
@@ -0,0 +1,47 @@
+/*
+Realizar un programa que simule el funcionamiento de un dispositivo
+RS232, este tipo de dispositivo lee cadenas enviadas por el usuario. Las
+cadenas deben llegar con un formato fijo: tienen que ser de un máximo
+de 5 caracteres de largo, el primer carácter tiene que ser X y el último
+tiene que ser una O.
+Las secuencias leídas que respeten el formato se consideran correctas,
+la secuencia especial "&&&&&" marca el final de los envíos (llamémosla
+FDE), y toda secuencia distinta de FDE, que no respete el formato se
+considera incorrecta.
+Al finalizar el proceso, se imprime un informe indicando la cantidad de
+lecturas correctas e incorrectas recibidas. Para resolver el ejercicio
+deberá investigar como se utilizan las siguientes funciones de Java
+Substring(), Length(), equals().
+ */
+package Guia3_Ejercicios;
+
+import java.util.Scanner;
+
+public class Ej7_guia3Ejercicios_FALTA {
+    
+    public static void main(String[] args) {
+        String frase;
+        int x, contCorrectas, contIncorrectos;
+        
+        Scanner leer = new Scanner(System.in);
+        
+        do{
+            System.out.println("Ingrese la frase de 5 caracteres y que empiece con X y teminte con O");
+            frase = leer.next();
+            
+            x = frase.length();
+            
+            if (x>5 || frase.substring(0,1).equalsIgnoreCase("X") != true || frase.substring(x-1).equalsIgnoreCase("O") != true) {
+                contIncorrectos = conIncorrectos + 1;
+            }
+            if () {
+                
+            }
+            
+            
+        }while();
+        
+        System.out.println(frase);
+    }
+   
+}
