d847382f231ae33f623f85292db57ef8c1ce8366
missefa <erfab@hotmail.com>
2023-05-02 23:32:02 -0300

    clase 2 mayo

---
+++ Java_guia9_Ej1Ejercicios/src/java_guia9_ej1ejercicios/Java_guia9_Ej1Ejercicios.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_guia9_ej1ejercicios;
+
+
+public class Java_guia9_Ej1Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
