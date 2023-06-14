376cd868bd98d0e28625fc5dc118e011f0c9e130
missefa <erfab@hotmail.com>
2023-06-14 00:57:24 -0300

    clase 13 de junio

---
+++ Java_guia12_Ej2Teoria/src/Entidades/Mensaje.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import Interfaces.DefinirMensaje;
+
+/**
+ *
+ * @author Erica
+ */
+public final class Mensaje implements DefinirMensaje {
+
+    @Override
+    public void metodo() {
+        System.out.println("Implementación del método");
+
+    }
+
+    
+    
+    
+}
---
+++ Java_guia12_Ej2Teoria/src/java_guia12_ej2teoria/Java_guia12_Ej2Teoria.java
@@ -0,0 +1,24 @@
+/*
+Vamos a crear una interfaz con un método abstracto. Luego desde el main intentaremos instanciar
+un objeto a partir de la interfaz
+ */
+package java_guia12_ej2teoria;
+
+import Entidades.Mensaje;
+
+
+/**
+ *
+ * @author Erica
+ */
+public class Java_guia12_Ej2Teoria {
+
+    public static void main(String[] args) {
+
+     Mensaje m = new Mensaje();
+     m.metodo();
+        
+        
+    }
+
+}
